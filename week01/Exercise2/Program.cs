692258c084ed6ec7d38f3024a9c6df9d38b9094f
Adenkou1 <adenkou1@gmail.com>
2025-01-16 06:01:01 -0800

    Going to apply sign

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Runtime.CompilerServices;
 
 class Program
 {
@@ -7,7 +8,29 @@ class Program
         Console.Write("Enter Your grade percentage Here: ");
         int grade = int.Parse(Console.ReadLine());
 
+
         string letter = "";
         string sign = "";
-    }
+
+         if (grade >= 90)
+        {
+            letter = "A";
+        }
+        else if (grade >= 80)
+        {
+            letter = "B";
+        }
+        else if (grade >= 70)
+        {
+            letter = "C";
+        }
+        else if (grade >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+    }   
 }
