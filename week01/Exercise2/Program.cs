0e95c9bdcea5d75857a49c3f017a45fdc3a42554
Adenkou1 <adenkou1@gmail.com>
2025-01-16 08:23:19 -0800

    Exercice2 completed

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -5,14 +5,46 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.Write("Enter Your grade percentage Here: ");
-        string valueFromUser = Console.ReadLine();
-        
-        int A = 90;
-        int B = 80;
-        int C = 70;
-        int D = 60;
-        int F = 50;
+        Console.Write("Enter your grade percentage: ");
+        double gradePercentage = Convert.ToDouble(Console.ReadLine());
+
+        string letter = "";
+        string sign = "";
+
+        if (gradePercentage >= 90)
+        {
+            letter = "A";
+        }
+        else if (gradePercentage >= 80)
+        {
+            letter = "B";
+        }
+        else if (gradePercentage >= 70)
+        {
+            letter = "C";
+        }
+        else if (gradePercentage >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+    
+        if (gradePercentage >= 70)
+        {
+            Console.WriteLine("Congratulations! You passed the course.");
+        }
+        else
+        {
+            Console.WriteLine("Keep working hard. You'll do better next time!");
+        }
+
+    
+        Console.WriteLine($"Your letter grade is: {letter}");
+    
         
     }
 }
