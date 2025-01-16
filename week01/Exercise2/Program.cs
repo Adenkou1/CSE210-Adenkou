a25b7245594e978c2df3b4b1024e4f36cb0881e3
Adenkou1 <adenkou1@gmail.com>
2025-01-16 05:31:07 -0800

    I will now determine letter grade

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,10 @@ class Program
 {
     static void Main(string[] args)
     {
-        
+        Console.Write("Enter Your grade percentage Here: ");
+        int grade = int.Parse(Console.ReadLine());
+
+        string letter = "";
+        string sign = "";
     }
 }
