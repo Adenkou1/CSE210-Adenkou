0927e737ea18ace3390417742ec80068a54ebfca
Adenkou1 <adenkou1@gmail.com>
2025-01-16 05:10:27 -0800

    Exercice 1 completed

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -7,6 +7,9 @@ class Program
         Console.Write("What is your first name? ");
         String first = Console.ReadLine();
 
-        
+        Console.Write("What is your last name? ");
+        String last = Console.ReadLine();
+
+        Console.WriteLine($"Your name is {last}, {first} {last}");
     }
 }
