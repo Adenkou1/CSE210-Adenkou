7ccecb81a4c510b142ed7787a9333dab2e32c1c7
Adenkou1 <adenkou1@gmail.com>
2025-01-16 06:38:04 -0800

    restart back my code Exer2

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -6,31 +6,7 @@ class Program
     static void Main(string[] args)
     {
         Console.Write("Enter Your grade percentage Here: ");
-        int grade = int.Parse(Console.ReadLine());
-
-
-        string letter = "";
-        string sign = "";
-
-         if (grade >= 90)
-        {
-            letter = "A";
-        }
-        else if (grade >= 80)
-        {
-            letter = "B";
-        }
-        else if (grade >= 70)
-        {
-            letter = "C";
-        }
-        else if (grade >= 60)
-        {
-            letter = "D";
-        }
-        else
-        {
-            letter = "F";
-        }
-    }   
+        
+        
+    }
 }
