54702f35bf705faa49dc14c5da29f8c2d1690142
Adenkou1 <adenkou1@gmail.com>
2025-01-16 08:19:36 -0800

    inputing Exerc2

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -6,7 +6,13 @@ class Program
     static void Main(string[] args)
     {
         Console.Write("Enter Your grade percentage Here: ");
+        string valueFromUser = Console.ReadLine();
         
+        int A = 90;
+        int B = 80;
+        int C = 70;
+        int D = 60;
+        int F = 50;
         
     }
 }
