72bde03acbcebbff120a1a049ac809ddad4fc6bd
Adenkou1 <adenkou1@gmail.com>
2025-01-25 15:30:09 -0800

    journal coding

--- week02/Journal/Program.cs
+++ week02/Journal/Program.cs
@@ -1,3 +1,24 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
+
+namespace JournalApp
+{
+    public class Entry 
+    {
+        public string Prompt { get; set;}
+        public string Response { get; set;}
+        public string Date { get; set;}
+
+        public Entry(string Prompt, string Response, string Date)
+        {
+            Prompt = prompt;
+            Response = response;
+            Date = Date;
+        }
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+    }
+}
