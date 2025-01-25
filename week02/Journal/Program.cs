dea0d20991e2ef4f5f14811a12439734454b66ac
Adenkou1 <adenkou1@gmail.com>
2025-01-25 15:19:56 -0800

    Journal coding startup

--- week02/Journal/Program.cs
+++ week02/Journal/Program.cs
@@ -1,9 +1,3 @@
 using System;
-
-class Program
-{
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello World! This is the Journal Project.");
-    }
-}
+using System.Collections.Generic;
+using System.IO;
