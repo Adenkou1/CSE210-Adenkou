f0d92f970abf421470a3f3e293effa786798fa7e
Adenkou1 <adenkou1@gmail.com>
2025-02-01 12:13:22 -0800

    Youtube Progrm in progress

--- week04/YouTubeVideos/Program.cs
+++ week04/YouTubeVideos/Program.cs
@@ -1,2 +1,21 @@
 using System;
+using System.Collections.Generic;
 
+
+class Comment 
+{
+    public string CommenterName { get; set;}
+    public string Text { get; set;}
+    
+    public Comment(string commenterName, string text)
+{CommenterName = commenterName; 
+Text = text;}
+}
+
+class Video
+{
+    public string Title { get; set;}
+    public string Author {get; set;}
+    public int Length { get; set;}
+    private List<Comment> comments;
+}
