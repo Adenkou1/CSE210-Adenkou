ab83eec73a3cbf6afd7b1e4d31a42fa78bd9eb49
Adenkou1 <adenkou1@gmail.com>
2025-02-06 08:45:02 -0800

    Mindfull program in progress

--- week05/Mindfulness/Program.cs
+++ week05/Mindfulness/Program.cs
@@ -1,3 +1,26 @@
 using System;
 using System.Collections.Generic;
 using System.Threading;
+
+//I will try to creat Base Class: Activity
+public class Activity
+{
+    protected string name;
+    protected string description;
+    protected int duration; //in minutes
+
+    public Activity(string name, string description, int duration)
+    {
+        this.name = name;
+        this.description = description;
+        this.duration = duration;
+    }
+
+    public void StartActivity()
+    {
+        Console.WriteLine("Starting " + name + "...");
+        Thread.Sleep(duration * 1000);
+        Console.WriteLine(name + " is done!");
+    }
+
+}
