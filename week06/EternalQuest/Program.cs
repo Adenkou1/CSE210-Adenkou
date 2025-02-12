bb3edeb68290aa729a394c0d8f5146f9c5deb429
Adenkou1 <adenkou1@gmail.com>
2025-02-12 11:02:04 -0800

    Eternal Quest in progress

--- week06/EternalQuest/Program.cs
+++ week06/EternalQuest/Program.cs
@@ -1,10 +1,23 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
-class Program
+
+// I'm about to create a new class: Base Goal
+abstract class Goal
 {
-    static void Main(string[] args)
+    protected string name;
+    protected string description;
+    protected int points;
+    protected bool isCompleted;
+
+    public Goal(string name, string description, int points)
     {
-        Console.WriteLine("Hello World! This is the EternalQuest Project.");
+        this.name = name;
+        this.description = description;
+        this.points = points;
+        isCompleted = false;
     }
+
+    public abstract void RecordEvent(ref int totalScore);
+   public virtual string GetDetailsString() => $"{name}" - {description} ({points} points);
 }
